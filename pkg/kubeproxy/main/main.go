ef613feb60bf0f6523e34240d138b25abf56dcb1
BryanWYP <wyp1536481268@foxmail.com>
2024-05-16 15:40:31 +0800

    [kubeproxy]: add main to fix bug

--- pkg/kubeproxy/main/main.go
+++ pkg/kubeproxy/main/main.go
@@ -1,7 +1,11 @@
 package main
 
-// func main() {
-// 	proxy := GETKubeproxy()
-// 	proxy.Run()
+import (
+	"minik8s/pkg/kubeproxy"
+)
 
-// }
+func main() {
+	proxy := kubeproxy.GetKubeproxy()
+	proxy.Run()
+
+}
