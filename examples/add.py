9d0a3cb99c1268f82e803b2a9fa637d81764abff
Liu Jialong <1149497708@qq.com>
2024-05-25 13:33:50 +0800

    Fix python script

--- examples/add.py
+++ examples/add.py
@@ -1,4 +1,16 @@
-def main(params):
-    x = params.get('x', 0)
-    y = params.get('y', 0)
-    return {'result': x + y}
+import sys
+
+def add_one(number):
+    return number + 1
+
+if __name__ == "__main__":
+    # 检查是否有命令行参数传入
+    if len(sys.argv) != 2:
+        print(0)
+    else:
+        try:
+            number = int(sys.argv[1])
+            result = add_one(number)
+            print(result)
+        except ValueError:
+            print(0)
