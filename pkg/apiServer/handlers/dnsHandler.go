9b8d6c673844244091cfab3153f7676fafad1b32
jackhel0 <102275313+jackhel0@users.noreply.github.com>
2024-05-30 22:58:12 +0800

    Update dnsHandler

--- pkg/apiServer/handlers/dnsHandler.go
+++ pkg/apiServer/handlers/dnsHandler.go
@@ -106,3 +106,38 @@ func AddDNS(c *gin.Context){
 
 	//TODO
 }
+
+func DeleteDNS(c *gin.Context){
+	name := c.Param("name")
+	namespace := c.Param("namespace")
+	if name == ""{
+		log.ErrorLog("DeleteDNS name is empty")
+		c.JSON(400, gin.H{"error": "name is empty"})
+		return
+	}
+	if namespace == ""{
+		namespace = "default"
+	}
+	log.InfoLog("DeleteDNS: "+namespace+"/"+name)
+	
+	key := config.EtcdDnsPrefix + "/" + namespace + "/" + name
+	oldRes, err := etcdclient.EtcdStore.Get(key)
+	if err != nil{
+		log.ErrorLog("DeleteDNS: "+err.Error())
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	if len(oldRes) == 0{
+		log.ErrorLog("DeleteDNS: not found")
+		c.JSON(404, gin.H{"error": "not found"})
+		return
+	}
+	err = etcdclient.EtcdStore.Delete(key)
+	if err != nil{
+		log.ErrorLog("DeleteDNS: "+err.Error())
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	//TODO dnsUpdate sync
+	c.JSON(200, gin.H{"data": "success"})
+}
