1a0d7f69046e0f1527a729edd5fb2d6daebf8e00
jackhel0 <102275313+jackhel0@users.noreply.github.com>
2024-05-31 14:26:05 +0800

    Add dnsController

--- pkg/controller/specCtlrs/dnsController.go
+++ pkg/controller/specCtlrs/dnsController.go
@@ -1,7 +1,13 @@
 package specctlrs
 
 import (
+	"encoding/json"
+	"minik8s/pkg/apiObject"
+	"minik8s/pkg/config"
 	"minik8s/tools/executor"
+	"minik8s/tools/log"
+	"net/http"
+	"strings"
 	"time"
 )
 
@@ -30,7 +36,68 @@ func (dc *DnsControllerImpl) Run() {
 }
 
 func (dc *DnsControllerImpl) syncDns() {
-	// 1. 获取所有的Pod
+	// 1. 获取所有的DnsRequest
+	dnsRequests, err := GetAllDnsRequest()
+	if err != nil {
+		log.ErrorLog("syncDns: " + err.Error())
+		return
+	}
+	for _, dnsRequest := range dnsRequests {
+		var dns apiObject.Dns
+		namespace := dnsRequest.DnsMeta.Namespace
+		name := dnsRequest.DnsMeta.Name
+		url := config.APIServerURL() + config.DnsRequestURI
+		url = strings.Replace(url, config.NameSpaceReplace, namespace, -1)
+		url = strings.Replace(url, config.NameReplace, name, -1)
+		res, err := http.Get(url)
+		if err != nil {
+			log.ErrorLog("syncDns: " + err.Error())
+			return
+		}
+		err = json.NewDecoder(res.Body).Decode(&dns)
+		requestType := dnsRequest.Action
+		switch requestType {
+			case "Create":
+				err = CreateDnsHandler(dns)
+			case "Delete":
+				err = DeleteDnsHandler(dns)
+		}
+		if err != nil {
+			log.ErrorLog("syncDns: " + err.Error())
+			return
+		}
+	}
 	// 2. 获取所有的Service
 	// 3. 更新DNS
+}
+
+func GetAllDnsRequest() (dnsRequests []apiObject.DnsRequest, err error) {
+	// 1. 获取所有的DnsRequest
+	url := config.APIServerURL() +config.GlobalDnsRequestURI
+	res,err := http.Get(url)
+	if err != nil{
+		log.ErrorLog("GetAllDnsRequest: "+err.Error())
+		return dnsRequests,err
+	}
+	if res.StatusCode != 200{
+		log.ErrorLog("GetAllDnsRequest: "+res.Status)
+		return dnsRequests,err
+	}
+	err = json.NewDecoder(res.Body).Decode(&dnsRequests)
+	if err != nil{
+		log.ErrorLog("GetAllDnsRequest: "+err.Error())
+		return dnsRequests,err
+	}
+	return dnsRequests,nil
+}
+
+
+func CreateDnsHandler(dns apiObject.Dns) error {
+	return nil;
+}
+
+func DeleteDnsHandler(dns apiObject.Dns) error {
+	// 1. 删除Dns
+	// 2. 创建DnsRequest
+	return nil
 }
