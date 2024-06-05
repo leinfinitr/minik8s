679029260706c5f0f2a1dd0e1f7dd10cd2a5f12a
Liu Jialong <1149497708@qq.com>
2024-06-05 14:57:28 +0800

    Fix again

--- examples/server/server.py
+++ examples/server/server.py
@@ -6,11 +6,7 @@ class CustomHTTPHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        try:
-            ifconfig_output = subprocess.check_output(['ifconfig'], text=True)
-            self.wfile.write(f'This is the ifconfig output:\n{ifconfig_output}'.encode())
-        except subprocess.CalledProcessError as e:
-            self.wfile.write(f'Error executing ifconfig: {e}'.encode())
+        self.wfile.write('This is the ifconfig output: '.encode() + os.environ['IFCONFIG_RESULT'].encode())
 
 if __name__ == '__main__':
     server_address = ('', 7080)
